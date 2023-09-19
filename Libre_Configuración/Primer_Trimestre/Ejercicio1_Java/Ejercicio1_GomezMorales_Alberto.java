72198d580a3a8d1c3697914ee6f7a3b27390fcbc
ElPediatra <albertogomezmorales1994@gmail.com>
2023-09-19 12:13:58 +0200

    Creacion Libre Configuración y primer ejercicio

---
+++ Libre_Configuración/Primer_Trimestre/Ejercicio1_Java/Ejercicio1_GomezMorales_Alberto.java
@@ -0,0 +1,20 @@
+/* Imports */
+import java.util.Scanner;
+
+/*
+ * Pedir por teclado una cadena:
+ * Mostrar menú:
+ *  - Mostrarla al revés
+ *  - Contar el nº de vocales
+ *  - Contar el nº de consonantes
+ *  - Pasar todo a minúscula
+ *  - Pasar todo a mayúscula
+ *  - Decir si es palíndroma
+ */
+
+public class Ejercicio1_GomezMorales_Alberto {
+    public static void main(String args[]){
+        
+    }
+
+}
