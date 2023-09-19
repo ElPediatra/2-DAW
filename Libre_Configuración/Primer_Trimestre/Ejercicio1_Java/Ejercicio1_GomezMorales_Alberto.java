77ff61417db56bae40baa7bd2c415d04e791f413
ElPediatra <albertogomezmorales1994@gmail.com>
2023-09-19 13:54:12 +0200

    Actualizaciones

--- Libre_Configuración/Primer_Trimestre/Ejercicio1_Java/Ejercicio1_GomezMorales_Alberto.java
+++ Libre_Configuración/Primer_Trimestre/Ejercicio1_Java/Ejercicio1_GomezMorales_Alberto.java
@@ -22,7 +22,6 @@ public class Ejercicio1_GomezMorales_Alberto {
         String palabra;
         int opcion;
 
-
         do{
             System.out.println("Escribe una palabra, o pon 'salir' para cerrar el programa");
             palabra = teclado.nextLine();
@@ -30,8 +29,55 @@ public class Ejercicio1_GomezMorales_Alberto {
             if(!palabra.equalsIgnoreCase("salir")){
                 System.out.println("Selecciona una de las opciones:");
                 System.out.println("1) Mostar la palabra al revés.");
-                System.out.println("2)");
+                System.out.println("2) Contar las vocales.");
+                System.out.println("3) Contar las consonantes.");
+                System.out.println("4) Pasar todo a minúsculas.");
+                System.out.println("5) Pasar todo a mayúsculas.");
+                System.out.println("6) Confirmar si es palíndroma.");
 
+                opcion = teclado.nextInt();
+
+                switch(opcion){
+                    case 1: //Invertir la palabra
+                        String palabraInvertida = "";
+                        char letra;
+
+                        for (int i = palabra.length() - 1; i >= 0; i--) {
+                            letra = palabra.charAt(i);
+                            palabraInvertida += letra;
+                        }
+
+                        System.out.println("La palabra '"+palabra+"' invertida es '"+palabraInvertida+"'.");
+                    break;
+                    
+                    case 2:
+                        int contador = 0;
+                        String palabraAux = palabra.toLowerCase();
+
+                        for(int i =0; i<palabra.length();i++){
+                            if ((palabraAux.charAt(i)=='a') || (palabraAux.charAt(i)=='e') || (palabraAux.charAt(i)=='i') || (palabraAux.charAt(i)=='o') || (palabraAux.charAt(i)=='u')){ 
+                                contador++;
+                            }
+                        }
+                        System.out.println("En la palabra '"+palabra+"' hay "+contador+" vocales.");
+                    break;
+
+                    case 3:
+                    break;
+
+                    case 4:
+                    break;
+
+                    case 5:
+                    break;
+
+                    case 6:
+                    break;
+
+                    default:
+                        System.out.println("Opción no válida.");
+                }
+                teclado.nextLine(); //Borrar palabra para evitar bucle infinito
             }
 
         }while(!palabra.equalsIgnoreCase("salir"));
