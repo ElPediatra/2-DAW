7a0020f5244deab3f24f04f1f464c61e740a9c54
ElPediatra <albertogomezmorales1994@gmail.com>
2023-09-29 12:12:01 +0200

    Ejercicio9

---
+++ Libre_Configuración/Primer_Trimestre/Ejercicio2_Java/Ejercicio9.java
@@ -0,0 +1,31 @@
+/* Alberto Gómez Morales - 2º DAW - Libre Configuración
+ * 
+ * 9.- Escriba una aplicación que reciba como entrada un código entero para un carácter y que muestre
+ * el carácter correspondiente.
+ */
+
+/* Imports */
+import java.util.Scanner;
+
+public class Ejercicio9 {
+    public static void main(String[] args){
+        /* Atributos */
+        int codigo;
+        char letra;
+
+        Scanner teclado = new Scanner(System.in);
+
+        //Pedimos el número a transformar
+        System.out.print("Escribe un número entero para una letra: ");
+        codigo = teclado.nextInt();
+
+        //Revisamos si el número está en el rango y lo transformamos
+        if (codigo >= 0 && codigo <= Character.MAX_CODE_POINT) {
+            letra = Character.toChars(codigo)[0];
+
+            System.out.println("La letra/simbolo que corresponde al número " + codigo + " es: " + letra);
+        } else {
+            System.out.println("El número indicado no es válido.");
+        }
+    }
+}
