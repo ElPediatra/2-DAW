4b9b6e5eee05d2d527d199863da89be9f53b175c
ElPediatra <albertogomezmorales1994@gmail.com>
2023-10-06 13:13:06 +0200

    Modificaciones

--- Desarrollo_Web_en_Entorno_Servidor/Primer_Trimestre/Tema2/Ejercicios_Cadenas_1/Ejercicio5.php
+++ Desarrollo_Web_en_Entorno_Servidor/Primer_Trimestre/Tema2/Ejercicios_Cadenas_1/Ejercicio5.php
@@ -1,7 +1,7 @@
 <?php
 
 $frase="Pagarás, pagarás, ya verás que pagarás";
-echo(substr_count($frase, ("a","á")));
+echo(substr_count($frase, "a","á"));
 
 //No hemos dado los arrays, se ha dicho que no se haga
 
