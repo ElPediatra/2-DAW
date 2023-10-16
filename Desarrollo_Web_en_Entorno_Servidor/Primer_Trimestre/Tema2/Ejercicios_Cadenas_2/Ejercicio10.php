44449b0c573dfb260167d4e640b8cdfca121a625
elpediatra <albertogomezmorales1994@gmail.com>
2023-10-16 21:35:03 +0200

    edit Ejercicio10

--- Desarrollo_Web_en_Entorno_Servidor/Primer_Trimestre/Tema2/Ejercicios_Cadenas_2/Ejercicio10.php
+++ Desarrollo_Web_en_Entorno_Servidor/Primer_Trimestre/Tema2/Ejercicios_Cadenas_2/Ejercicio10.php
@@ -1,3 +1,3 @@
-<?php
+<?php //Muestro la cadena de texto que me han pedido con echo
 echo '<a href="/arbol/prueba.php" class="prueba" onmouseover="status=\'hola\'; return trae;">prueba de enlace</a>';
 ?>
