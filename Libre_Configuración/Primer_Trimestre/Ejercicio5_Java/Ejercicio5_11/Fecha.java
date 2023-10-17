160e202565b14ef5b2b893868c969abb6e45039f
ElPediatra <albertogomezmorales1994@gmail.com>
2023-10-17 13:38:14 +0200

    Ejercicio5_11

---
+++ Libre_Configuración/Primer_Trimestre/Ejercicio5_Java/Ejercicio5_11/Fecha.java
@@ -0,0 +1,94 @@
+/* Paquetes */
+package Libre_Configuración.Primer_Trimestre.Ejercicio5_Java.Ejercicio5_11;
+
+public class Fecha {
+    /* Atributos */
+    private int mes;
+    private int dia;
+    private int anio;
+
+    /* Sub-clase meses */
+    private static final String[] MESES = {
+        "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+        "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
+    };
+
+    /* Constructores */
+    public Fecha(int mes, int dia, int anio) {
+        this.mes = mes;
+        this.dia = dia;
+        this.anio = anio;
+    }
+
+    public Fecha(String mesStr, int dia, int anio) {
+        this.mes = convertirMesStringAMesNumero(mesStr);
+        this.dia = dia;
+        this.anio = anio;
+    }
+
+    public Fecha(int diaDelAnio, int anio) {
+        this.anio = anio;
+        int dia = 1;
+        int mes = 0;
+        while (diaDelAnio > diasEnMes(mes, anio)) {
+            diaDelAnio -= diasEnMes(mes, anio);
+            mes++;
+        }
+        this.mes = mes;
+        this.dia = diaDelAnio;
+    }
+
+    /* Métodos */
+
+    //"to Strings" dependiendo del formato
+    public void imprimirFormato1() {
+        System.out.printf("%s %d, %d%n", MESES[mes - 1], dia, anio);
+    }
+
+    public void imprimirFormato2() {
+        System.out.printf("%s %d%n", obtenerNombreDiaDeLaSemana(), anio);
+    }
+
+    //Sacar el día por texto
+    private String obtenerNombreDiaDeLaSemana() {
+        int diaDeLaSemana = diaDeLaSemana();
+        String[] diasSemana = {"Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"};
+        return diasSemana[diaDeLaSemana];
+    }
+
+    //Sacar el día de la semana
+    private int diaDeLaSemana() {
+        int a = (14 - mes) / 12;
+        int y = anio - a;
+        int m = mes + 12 * a - 2;
+        return (dia + y + y / 4 - y / 100 + y / 400 + (31 * m) / 12) % 7;
+    }
+
+    //Confirmar los días que tiene un mes
+    private int diasEnMes(int mes, int anio) {
+        if (mes == 1) {
+            if (esBisiesto(anio)) {
+                return 29;
+            } else {
+                return 28;
+            }
+        } else {
+            return new int[] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }[mes];
+        }
+    }
+
+    //Confirmar si es bisiesto
+    private boolean esBisiesto(int anio) {
+        return (anio % 4 == 0 && anio % 100 != 0) || (anio % 400 == 0);
+    }
+
+    //Pasar string de mes a texto
+    private int convertirMesStringAMesNumero(String mesStr) {
+        for (int i = 0; i < MESES.length; i++) {
+            if (MESES[i].equalsIgnoreCase(mesStr)) {
+                return i + 1; //es +1 porque empieza en 0
+            }
+        }
+        return -1; //Devuelvo -1 si el mes no existe
+    }
+}
--- Libre_Configuración/Primer_Trimestre/Ejercicio5_Java/Ejercicio5_11/Principal.java
+++ Libre_Configuración/Primer_Trimestre/Ejercicio5_Java/Ejercicio5_11/Principal.java
@@ -1,5 +1,37 @@
+/* Alberto Gómez Morales - 2º DAW - Libre Configuración
+ * 
+ * 11.- (Clase Fecha) Cree la clase Fecha con las siguientes capacidades:
+ *  a) Imprimir la fecha en varios formatos, como
+ *      MM,'DD/AAAA
+ *      Junio 15, 1992
+ *      DDD AAAA
+ *  b) Usar constructores sobrecargados para crear objetos Fecha inicializados con fechas de los
+ * formatos en la parte (a). En el primer caso, el constructor debe recibir tres valores enteros.
+ * En el segundo caso, debe recibir un objeto String y dos valores enteros. En el tercer caso
+ * debe recibir dos valores enteros, eI primero de los cuales representa eI número de día en eI
+ * año, [Sugerencia: para convertir la representación de cadena del mes a un valor numérico,
+ * compare las cadenas usando el método equals . Por ejemplo, Si SI y s2 son cadenas, la
+ * llamada al método sl .equals( s2 ) devuelve true si las cadenas son idénticas y devuelve false
+ * en cualquier otro caso].
+ */
+
+/* Paquetes */
 package Libre_Configuración.Primer_Trimestre.Ejercicio5_Java.Ejercicio5_11;
 
 public class Principal {
-    
+    public static void main(String[] args) {
+
+        /* Creo los objetos Fecha y los muestro de las 2 maneras que faltan */
+        Fecha fecha1 = new Fecha(6, 15, 1992);
+        fecha1.imprimirFormato1();
+        fecha1.imprimirFormato2();
+
+        Fecha fecha2 = new Fecha("Junio", 15, 1992);
+        fecha2.imprimirFormato1();
+        fecha2.imprimirFormato2();
+
+        Fecha fecha3 = new Fecha(167, 1992);
+        fecha3.imprimirFormato1();
+        fecha3.imprimirFormato2();
+    }
 }
