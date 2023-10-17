267d2cc9256e1da318729785bc082c8f31aaef42
ElPediatra <albertogomezmorales1994@gmail.com>
2023-10-17 19:20:09 +0200

    Ejercicio3_11

---
+++ Desarrollo_Web_en_Entorno_Servidor/Primer_Trimestre/Tema2/Ejercicios_Cadenas_3/Ejercicio3_11.php
@@ -0,0 +1,15 @@
+<?php
+$cadena = "Hola grupo, soy Vanesa";
+$caracteres = str_split($cadena);
+
+//Convierno $caracteres en un array con str_splity donde cada elemento es un carácter de la cadena
+
+//Creo un bucle y muestro los caracteres del array
+foreach ($caracteres as $caracter) {
+    echo $caracter . " ";
+}
+
+//También puedo acceder a un elemento en específico por su índice:
+echo $caracteres[0]; //Para 0 tiene que mostrar "H"
+
+?>
