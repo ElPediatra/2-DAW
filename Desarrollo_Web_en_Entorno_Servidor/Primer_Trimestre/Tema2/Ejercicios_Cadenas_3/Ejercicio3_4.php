fe18698c5dfadb8551fd9f33846287e6363f0e61
ElPediatra <albertogomezmorales1994@gmail.com>
2023-10-17 18:26:41 +0200

    Fix Ejercicio3_4

--- Desarrollo_Web_en_Entorno_Servidor/Primer_Trimestre/Tema2/Ejercicios_Cadenas_3/Ejercicio3_4.php
+++ Desarrollo_Web_en_Entorno_Servidor/Primer_Trimestre/Tema2/Ejercicios_Cadenas_3/Ejercicio3_4.php
@@ -12,26 +12,26 @@ $resultado2 = strncmp($cadena1, $cadena2, 5);
 $resultado3 = strnatcmp($cadena1, $cadena2);
 
 if ($resultado1 < 0) {
-    echo "La cadena 1 es menor que la cadena 2 (comparación de toda la cadena).\n";
+    echo "La cadena 1 es menor que la cadena 2 (comparación de toda la cadena).<br>";
 } elseif ($resultado1 > 0) {
-    echo "La cadena 2 es menor que la cadena 1 (comparación de toda la cadena).\n";
+    echo "La cadena 2 es menor que la cadena 1 (comparación de toda la cadena).<br>";
 } else {
-    echo "Las 2 cadenas son iguales (comparación de toda la cadena).\n";
+    echo "Las 2 cadenas son iguales (comparación de toda la cadena).<br>";
 }
 
 if ($resultado2 < 0) {
-    echo "La cadena 1 es menor que la cadena 2 (comparación de los primeros 5 caracteres).\n";
+    echo "La cadena 1 es menor que la cadena 2 (comparación de los primeros 5 caracteres).<br>";
 } elseif ($resultado2 > 0) {
-    echo "La cadena 2 es menor que la cadena 1 (comparación de los primeros 5 caracteres).\n";
+    echo "La cadena 2 es menor que la cadena 1 (comparación de los primeros 5 caracteres).<br>";
 } else {
-    echo "Las 2 cadenas son iguales en los primeros 5 caracteres (comparación de los primeros 5 caracteres).\n";
+    echo "Las 2 cadenas son iguales en los primeros 5 caracteres (comparación de los primeros 5 caracteres).<br>";
 }
 
 if ($resultado3 < 0) {
-    echo "La cadena 1 es menor que la cadena 2 (comparación natural).\n";
+    echo "La cadena 1 es menor que la cadena 2 (comparación natural).<br>";
 } elseif ($resultado3 > 0) {
-    echo "La cadena 2 es menor que la cadena 1 (comparación natural).\n";
+    echo "La cadena 2 es menor que la cadena 1 (comparación natural).<br>";
 } else {
-    echo "Las 2 cadenas son iguales en una comparación natural.\n";
+    echo "Las 2 cadenas son iguales en una comparación natural.<br>";
 }
 ?>
