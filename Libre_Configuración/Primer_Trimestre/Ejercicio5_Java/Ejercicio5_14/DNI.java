f3a635c0589e59a1124c155309eeff8d27239059
ElPediatra <albertogomezmorales1994@gmail.com>
2023-10-17 14:28:55 +0200

    Ejercicio 5_14

---
+++ Libre_Configuración/Primer_Trimestre/Ejercicio5_Java/Ejercicio5_14/DNI.java
@@ -0,0 +1,103 @@
+/* Paquetes */
+package Libre_Configuración.Primer_Trimestre.Ejercicio5_Java.Ejercicio5_14;
+
+public class DNI {
+    /* Atributos */
+    private int numeroDni;
+
+    /* Constructores */
+    public DNI() {
+        //Constructor vacío
+    }
+
+    public DNI(int numeroDni) throws IllegalArgumentException {
+        if (validarNumeroDNI(numeroDni)) {
+            this.numeroDni = numeroDni;
+        } else {
+            throw new IllegalArgumentException("Número de DNI incorrecto.");
+        }
+    }
+
+    public DNI(String nif) throws IllegalArgumentException {
+        if (validarNIF(nif)) {
+            this.numeroDni = extraerNumeroDNI(nif);
+        } else {
+            throw new IllegalArgumentException("NIF incorrecto.");
+        }
+    }
+
+    /* Setter y Getters */
+    public void setDNI(int numeroDni) throws IllegalArgumentException {
+        if (validarNumeroDNI(numeroDni)) {
+            this.numeroDni = numeroDni;
+        } else {
+            throw new IllegalArgumentException("Número de DNI incorrecto.");
+        }
+    }
+
+    public void setDNI(String nif) throws IllegalArgumentException {
+        if (validarNIF(nif)) {
+            this.numeroDni = extraerNumeroDNI(nif);
+        } else {
+            throw new IllegalArgumentException("NIF incorrecto.");
+        }
+    }
+
+    public int getNumeroDni() {
+        return numeroDni;
+    }
+
+    public String getNif() {
+        return calcularNIF(numeroDni);
+    }
+
+    /* Métodos */
+
+    //Confirmar la letra del DNI
+    private static char calcularLetraNIF(int numeroDni) {
+        String letras = "TRWAGMYFPDXBNJZSQVHLCKE";
+        int indice = numeroDni % 23;
+        return letras.charAt(indice);
+    }
+
+    //Validar número del DNI
+    private static boolean validarNumeroDNI(int numeroDni) {
+        return numeroDni >= 0 && numeroDni <= 99999999;
+    }
+
+    //Validar DNI completo
+    private static boolean validarNIF(String nif) {
+        if (nif.length() == 9) {
+            char letraCalculada = calcularLetraNIF(extraerNumeroDNI(nif));
+            char letraNIF = nif.charAt(8);
+            return letraCalculada == letraNIF;
+        }
+        return false;
+    }
+
+    //Obtener la letra del DNI
+    private static char extraerLetraNIF(String nif) {
+        if (nif.length() == 9) {
+            return nif.charAt(8);
+        }
+        return ' ';
+    }
+
+    //Obtener el número del DNI
+    private static int extraerNumeroDNI(String nif) {
+        if (nif.length() == 9) {
+            try {
+                return Integer.parseInt(nif.substring(0, 8));
+            } catch (NumberFormatException e) {
+                return -1;
+            }
+        }
+        return -1;
+    }
+
+    //Sacar el DNI completo
+    private String calcularNIF(int numeroDni) {
+        char letraNIF = calcularLetraNIF(numeroDni);
+        return String.format("%08d%c", numeroDni, letraNIF);
+    }
+}
--- Libre_Configuración/Primer_Trimestre/Ejercicio5_Java/Ejercicio5_14/Principal.java
+++ Libre_Configuración/Primer_Trimestre/Ejercicio5_Java/Ejercicio5_14/Principal.java
@@ -1,5 +1,49 @@
+/* Alberto Gómez Morales - 2º DAW - Libre Configuración
+ *
+ * 14.- (DNI) Se desea crear una clase que represente un DNI español y que tenga las siguientes
+ * características:
+ * 
+ * • La clase almacenará el número de DNI en un int, sin guardar la letra, pues se puede calcular
+ * a partir del número. Este atributo será privado a la clase. Formato del atributo: private int
+ * numeroDni.
+ * 
+ * • Para acceder al DNI se dispondrá de dos métodos get, uno que proporcionará el número de
+ * DNI (sólo las cifras numéricas) y Otro que devolverá el NIF completo (incluida la letra). EI
+ * formato del método será:
+ *  public int getNumeroDni ()
+ *  public String getNif O
+ *
+ * • Para modificar el DNI se dispondrá de dos métodos set, que permitirán modificar el DNI.
+ * Uno en el que habrá que proporcionar el NIF completo (número y letra). Y otro en el que
+ * únicamente será necesario proporcionar el DNI (las siete u ocho cifras). Si el DNI/NIF es
+ * incorrecto se debería lanzar algún tipo de excepción. EI formato de los métodos
+ * (sobrecargados) será:
+ *  public void setDni (String nif) throws
+ *  public void setDni (int numeroDni) throws
+ * 
+ * • La clase dispondrá de algunos métodos internos privados para calcular la letra de un número
+ * de DNI cualquiera, para comprobar Si un DNI con Su letra es válido, para extraer la letra de
+ * un NIF, Aquellos métodos que no utilicen ninguna variable de objeto podrían declararse
+ * como estáticos (pertenecientes a la clase). Formato de los métodos:
+ *  private static char calcularLetraNIF(int numeroDni)
+ *  private static boolean validarNif (String nif)
+ *  private Static Char extraerLetraNif (String nif)
+ *  private static int extraerNumeroDni (String nif)
+*/
+
+/* Paquetes */
 package Libre_Configuración.Primer_Trimestre.Ejercicio5_Java.Ejercicio5_14;
 
 public class Principal {
-    
+    public static void main(String[] args){
+        //Creo un objeto DNI y lo muestro
+        DNI dni1 = new DNI(12345678);
+        System.out.println("Número de DNI: " + dni1.getNumeroDni());
+        System.out.println("NIF: " + dni1.getNif());
+
+        //Cambio de DNI a NIF
+        dni1.setDNI("87654321H");
+        System.out.println("Número de DNI modificado: " + dni1.getNumeroDni());
+        System.out.println("NIF modificado: " + dni1.getNif());
+    }
 }
