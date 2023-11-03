58f378a0f959f9b8268d369d690dc16fc1e4456e
elpediatra <albertogomezmorales1994@gmail.com>
2023-11-03 10:51:41 +0100

    Ejercicios Ficha4 (muevo a la misma carpeta)

---
+++ Libre_Configuración/Primer_Trimestre/Ejercicio6_Java/Ejercicio_9_Ficha4.java
@@ -0,0 +1,10 @@
+/* Alberto Gómez Morales - 2º DAW - Libre Configuración
+ * 
+ * 9.- Escriba un método llamado enteroPotencia( base, exponente ) que devuelva el valor de base(elevado a exponente)
+ * Por ejemplo, enteroPotencia( 3, 4 ) calcula 34 (0 3 * 3 * 3 * 3 ). Suponga que exponente es un entero
+ * positivo distinto de cero y que base es un entero. El método enteroPotencia debe utilizar un ciclo for
+ * o while para controlar el calculo. No utilice ningún método de la biblioteca de matemáticas.
+ * Incorpore este método en una aplicación que lea valores enteros para base y exponente, y que
+ * realice el calculo con el método enteroPotencia. Realice ahora el método enteroPotenciaRecursivo
+ * que realizará el mismo cálculo pero de forma recursiva.
+ */
