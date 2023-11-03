248e1f06deee72830a13cc92226e9435d34f00a8
elpediatra <albertogomezmorales1994@gmail.com>
2023-11-03 12:25:13 +0100

    Ejercicio_9_Ficha4

--- Libre_Configuración/Primer_Trimestre/Ejercicio6_Java/Ejercicio_9_Ficha4.java
+++ Libre_Configuración/Primer_Trimestre/Ejercicio6_Java/Ejercicio_9_Ficha4.java
@@ -8,3 +8,53 @@
  * realice el calculo con el método enteroPotencia. Realice ahora el método enteroPotenciaRecursivo
  * que realizará el mismo cálculo pero de forma recursiva.
  */
+
+/* Imports */
+import java.util.Scanner;
+
+public class Ejercicio_9_Ficha4 {
+    public static void main(String[] args) {
+        /* Atributos */
+        Scanner teclado = new Scanner(System.in);
+        int base;
+        int exponente;
+        int resultado;
+        int resultadoRecursivo;
+
+        //Pido la base y el exponente al usuario
+        System.out.print("Escribe la base: ");
+        base = teclado.nextInt();
+
+        System.out.print("Escribe el exponente: ");
+        exponente = teclado.nextInt();
+
+        //Llamo a los método para calcular el resultado y lo muestro
+        resultado = enteroPotencia(base, exponente);
+        resultadoRecursivo = enteroPotenciaRecursivo(base, exponente);
+
+        System.out.println(base + " elevado a " + exponente + " (iterativo) es igual a " + resultado);
+        System.out.println(base + " elevado a " + exponente + " (recursivo) es igual a " + resultadoRecursivo);
+
+        teclado.close();
+    }
+
+    /* Métodos */
+    public static int enteroPotencia(int base, int exponente) {
+        int resultado = 1;
+
+        for (int i = 0; i < exponente; i++) {
+            resultado *= base;
+        }
+
+        return resultado;
+    }
+
+    public static int enteroPotenciaRecursivo(int base, int exponente) {
+        if (exponente == 0) {
+            return 1;
+        } else {
+            return base * enteroPotenciaRecursivo(base, exponente - 1);
+        }
+    }
+}
+
