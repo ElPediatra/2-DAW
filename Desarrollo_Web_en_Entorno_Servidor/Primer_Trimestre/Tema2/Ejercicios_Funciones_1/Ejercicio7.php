a91495272d97d271bea458c0beeda038e2c91aba
ElPediatra <albertogomezmorales1994@gmail.com>
2023-11-11 18:38:46 +0100

    Funciones 1 - Ejercicio 7

---
+++ Desarrollo_Web_en_Entorno_Servidor/Primer_Trimestre/Tema2/Ejercicios_Funciones_1/Ejercicio7.php
@@ -0,0 +1,12 @@
+<?php
+function cuadrado($num) {
+    return $num * $num;
+}
+
+function cubo($num) {
+    return $num * $num * $num;
+}
+
+echo cuadrado(4);  //Tiene que mostrar 16
+echo cubo(3);      //Tiene que mostrar 27
+?>
