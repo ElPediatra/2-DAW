d1371d8bf8269918dfc19f793b54d4cc03b85885
elpediatra <albertogomezmorales1994@gmail.com>
2023-11-13 09:43:25 +0100

    documentación Objeto_Usuario y comienzo Fibonacci

--- Desarrollo_Web_en_Entorno_Cliente/Primer_Trimestre/Tema 3/Objeto_Usuario/Objeto_Usuario.js
+++ Desarrollo_Web_en_Entorno_Cliente/Primer_Trimestre/Tema 3/Objeto_Usuario/Objeto_Usuario.js
@@ -3,12 +3,14 @@
 document.getElementById('login').addEventListener('submit', function(event) {
     event.preventDefault();
 
+    /* Constructor por defecto */
     var Usuario = function(usuario, contraseña) {
         this.usuario = usuario;
         this.contraseña = contraseña;
     }
 
-    Usuario.prototype.checkInformation = function() {
+    /* Validador del usuario (se supone que ya está creado, solo estamos probando el login con función) */
+    Usuario.prototype.checkInformation = function() { //Método booleano para validar el usuario
         if (this.usuario === 'admin' && this.contraseña === 'admin') {
             return true;
         } else {
@@ -16,11 +18,13 @@ document.getElementById('login').addEventListener('submit', function(event) {
         }
     }
 
+    /* Creo las variables y el nuevo usuario */
     var usuario = document.getElementById('usuario').value;
     var contraseña = document.getElementById('contraseña').value;
 
     var nuevoUsuario = new Usuario(usuario, contraseña);
 
+    /* Hago la comparación con el método booleano */
     if (nuevoUsuario.checkInformation()) {
         document.getElementById('mensaje').textContent = 'Usuario correcto, iniciando sesion...';
     } else {
