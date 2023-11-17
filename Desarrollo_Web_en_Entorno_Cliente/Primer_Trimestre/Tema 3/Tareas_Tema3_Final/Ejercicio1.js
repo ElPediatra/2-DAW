64d94b246aafe10f45528e3e7495804ea28d1d70
elpediatra <albertogomezmorales1994@gmail.com>
2023-11-17 09:21:45 +0100

    Ejercicio html y js

---
+++ Desarrollo_Web_en_Entorno_Cliente/Primer_Trimestre/Tema 3/Tareas_Tema3_Final/Ejercicio1.js
@@ -0,0 +1,21 @@
+// Obtener la hora actual
+var horaActual = new Date().getHours();
+
+// Generar un número aleatorio entre 0 y 1
+var numeroAleatorio = Math.random();
+
+// Obtener el elemento HTML por su ID
+var mensajeElemento = document.getElementById("mensaje");
+
+// Comprobar si el número aleatorio es menor que 0.5
+if (numeroAleatorio < 0.5) {
+    // Si es menor que 0.5, mostrar enlace a Myfpschool.com
+    mensajeElemento.innerHTML = '<a href="https://www.myfpschool.com">Myfpschool.com</a>';
+} else {
+    // Si es mayor o igual a 0.5, mostrar saludo según la hora actual
+    if (horaActual < 15) {
+        mensajeElemento.innerHTML = 'Buenos días';
+    } else {
+        mensajeElemento.innerHTML = 'Buenas tardes';
+    }
+}
