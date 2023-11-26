8e133286ca0c9ebd989b7cf97b867e69f4811590
ElPediatra <albertogomezmorales1994@gmail.com>
2023-11-26 13:08:09 +0100

    Ejercicio 2 Tema 3 (Propuestos)

--- Desarrollo_Web_en_Entorno_Cliente/Primer_Trimestre/Tema 3/Tareas_Tema3_Propuestos/Ejercicio2.js
+++ Desarrollo_Web_en_Entorno_Cliente/Primer_Trimestre/Tema 3/Tareas_Tema3_Propuestos/Ejercicio2.js
@@ -8,12 +8,7 @@ function Coche(modelo, color, kms, combustible) {
 var miCoche = new Coche("Mercedes E330", "negro", 120000, "diésel");
 var tuCoche = new Coche("BMW 318", "blanco", 210000, "gasolina");
 
-// Intenta borrar un atributo que no existe
+//El atributo matrícula no existe, por lo que no borrará nada del objeto
 console.log("Antes de borrar:", miCoche);
-delete miCoche.matricula; // Intenta borrar un atributo incorrecto
+delete miCoche.matricula;
 console.log("Después de intentar borrar:", miCoche);
-
-// Intenta borrar un atributo existente
-console.log("Antes de borrar:", tuCoche);
-delete tuCoche.kms; // Borra el atributo "kms"
-console.log("Después de borrar:", tuCoche);
