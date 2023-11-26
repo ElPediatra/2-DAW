06daec2ed45a7c55a63c5dacd89ae1ace1611ac4
ElPediatra <albertogomezmorales1994@gmail.com>
2023-11-26 13:58:06 +0100

    Ejercicio 3

---
+++ Desarrollo_Web_en_Entorno_Cliente/Primer_Trimestre/Tema 3/Tareas_Tema3_Final/Ejercicio3.js
@@ -0,0 +1,17 @@
+function buscarUltimaPosicion() {
+    //Capturo la frase y la palabra
+    var frase = document.getElementById('fraseEntrada').value;
+    var palabra = document.getElementById('palabraEntrada').value;
+
+    //Compruebo que no están vacías
+    if (frase.trim() === '' || palabra.trim() === '') {
+        alert('Por favor, ingresa una frase y una palabra.');
+        return;
+    }
+
+    //Busco la última posición de la palabra usando LastIndexOf()
+    var ultimaPosicion = frase.toLowerCase().lastIndexOf(palabra.toLowerCase());
+
+    //Mostrar el resultado
+    document.getElementById('resultado').innerText = `La última posición de "${palabra}" es: ${ultimaPosicion}`;
+}
