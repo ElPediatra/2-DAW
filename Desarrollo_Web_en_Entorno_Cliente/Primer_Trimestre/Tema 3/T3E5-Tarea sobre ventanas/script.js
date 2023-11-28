ee652d729c2b231f003ca16720b42b9cd1dbceef
elpediatra <albertogomezmorales1994@gmail.com>
2023-11-28 14:30:04 +0100

    T3E5-Tarea sobre ventanas Completo

--- Desarrollo_Web_en_Entorno_Cliente/Primer_Trimestre/Tema 3/T3E5-Tarea sobre ventanas/script.js
+++ Desarrollo_Web_en_Entorno_Cliente/Primer_Trimestre/Tema 3/T3E5-Tarea sobre ventanas/script.js
@@ -1,17 +1,66 @@
+//Variable para capturar la ventana secundaria
 let ventanaSecundaria;
 
-function abrirPaginaSecundaria() {
-   // Abre la página secundaria y almacena la referencia a la ventana en la variable global
-   ventanaSecundaria = window.open("secundaria.html", "_blank", "width=400,height=300");
+//Función para abrir la ventana secundaria
+function abrirVentanaSecundaria() {
+    ventanaSecundaria = window.open("secundaria.html", "_blank", "width=400,height=300");
 }
 
-function solicitarCoordenadas() {
-   // Pide las coordenadas de la ventana secundaria y las almacena en variables
-   const nuevaAltura = prompt("Introduce la nueva altura de la ventana secundaria:");
-   const nuevaAnchura = prompt("Introduce la nueva anchura de la ventana secundaria:");
+//Función para cerrar la ventana secundaria desde la principal
+function cerrarVentanaSecundaria() {
+    if (ventanaSecundaria) {
+        ventanaSecundaria.close();
+    }
+}
 
-   // Aplica las coordenadas si son válidas
-   if (ventanaSecundaria && !ventanaSecundaria.closed && !isNaN(nuevaAltura) && !isNaN(nuevaAnchura)) {
-      ventanaSecundaria.resizeTo(Number(nuevaAnchura), Number(nuevaAltura));
-   }
+//Función para obtener y aplicar coordenadas a la ventana secundaria
+function aplicarCoordenadas() {
+    if (ventanaSecundaria) {
+        const x = prompt("Escribe la coordenada X:");
+        const y = prompt("Escribe la coordenada Y:");
+        ventanaSecundaria.moveTo(x, y);
+    }
+}
+
+//Función para cambiar el color de fondo de la ventana secundaria
+function cambiarColorFondo() {
+    if (ventanaSecundaria) {
+        const color = document.getElementById("colorSelector").value;
+        ventanaSecundaria.document.body.style.backgroundColor = color;
+    }
+}
+
+//Función para enfocar la ventana secundaria
+function enfocarVentana() {
+    if (ventanaSecundaria) {
+        ventanaSecundaria.focus();
+    }
+}
+
+//Función para quitar el enfoque de la ventana secundaria
+function quitarEnfoqueVentana() {
+    if (ventanaSecundaria) {
+        ventanaSecundaria.blur();
+    }
+}
+
+//Función para establecer el tiempo de actividad de la ventana secundaria
+function establecerTiempoActividad() {
+    if (ventanaSecundaria) {
+        const tiempo = prompt("Escribe el tiempo en segundos que quieres que esté la ventana abierta:");
+        setTimeout(() => {
+            ventanaSecundaria.close();
+        }, tiempo * 1000);
+    }
+}
+
+//Función para establecer el texto y estilo en la ventana secundaria
+function establecerTextoEstilo() {
+    if (ventanaSecundaria) {
+        const texto = prompt("Escribe el texto que quieres poner:");
+        const tamañoTexto = prompt("Indicame el tamaño del texto:");
+        const colorTexto = prompt("Indicame el color del texto (en inglés):");
+        
+        ventanaSecundaria.document.body.innerHTML = `<h1 style="font-size: ${tamañoTexto}px; color: ${colorTexto};">${texto}</h1>`;
+    }
 }
