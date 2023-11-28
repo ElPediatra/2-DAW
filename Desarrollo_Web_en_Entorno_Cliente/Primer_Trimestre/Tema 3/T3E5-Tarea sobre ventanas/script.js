f496074b5d8c5f001cb4f9f75351b2ca822e68ce
elpediatra <albertogomezmorales1994@gmail.com>
2023-11-28 12:09:29 +0100

    edit js

--- Desarrollo_Web_en_Entorno_Cliente/Primer_Trimestre/Tema 3/T3E5-Tarea sobre ventanas/script.js
+++ Desarrollo_Web_en_Entorno_Cliente/Primer_Trimestre/Tema 3/T3E5-Tarea sobre ventanas/script.js
@@ -1,72 +1,17 @@
-var parametros = "width=400, height=300, left=200, top=150, resizable=no, menubar=no, toolbar=no, directories=no, location=no, scrollbars=no, status=no";
-var ventanaSecundaria;
+let ventanaSecundaria;
 
-function abrirVentana() {
-    ventanaSecundaria = window.open("secundaria.html", "ventana", parametros);
-    console.log(ventanaSecundaria);
+function abrirPaginaSecundaria() {
+   // Abre la página secundaria y almacena la referencia a la ventana en la variable global
+   ventanaSecundaria = window.open("secundaria.html", "_blank", "width=400,height=300");
 }
 
-function cerrarVentanaSecundaria() {
-    if (ventanaSecundaria) {
-        ventanaSecundaria.close();
-    }
-}
+function solicitarCoordenadas() {
+   // Pide las coordenadas de la ventana secundaria y las almacena en variables
+   const nuevaAltura = prompt("Introduce la nueva altura de la ventana secundaria:");
+   const nuevaAnchura = prompt("Introduce la nueva anchura de la ventana secundaria:");
 
-function pedirCoordenadas() {
-    var left = prompt("Escribe la coordenada left para la ventana secundaria:", "200");
-    var top = prompt("Escribe la coordenada top para la ventana secundaria:", "150");
-    
-    if (!isNaN(left) && !isNaN(top)) {
-        parametros = "width=400, height=300, left=" + left + ", top=" + top + ", resizable=no, menubar=no, toolbar=no, directories=no, location=no, scrollbars=no, status=no";
-        alert("Coordenadas aplicadas con éxito.");
-    } else {
-        alert("Escribe valores en números válidos por favor.");
-    }
-}
-
-function seleccionarColores() {
-    var colorSelector = document.getElementById("colorSelector");
-    var selectedColor = colorSelector.options[colorSelector.selectedIndex].value;
-
-    document.body.style.backgroundColor = selectedColor;
-    if (ventanaSecundaria) {
-        ventanaSecundaria.document.body.style.backgroundColor = selectedColor;
-    }
-}
-
-function aplicarFocus() {
-    if (ventanaSecundaria) {
-        ventanaSecundaria.focus();
-    }
-}
-
-function aplicarBlur() {
-    if (ventanaSecundaria) {
-        ventanaSecundaria.blur();
-    }
-}
-
-function tiempoActivacion() {
-    var tiempo = prompt("Escribe el tiempo de duración para la ventana secundaria (en milisegundos):", "5000");
-    
-    if (!isNaN(tiempo)) {
-        setTimeout(function() {
-            cerrarVentanaSecundaria();
-        }, tiempo);
-    } else {
-        alert("Escribe valores en números válidos por favor.");
-    }
-}
-
-function textoVentana() {
-    var texto = prompt("Escribe el texto que quieres mostrar en la ventana secundaria:", "Hola, esta es la ventana secundaria.");
-    ventanaSecundaria.document.getElementById("mensaje").innerHTML = texto;
-}
-
-function estiloTextoVentana() {
-    var tamañoTexto = prompt("Escribe el tamaño de texto para la ventana secundaria:", "16");
-    var colorTexto = prompt("Escribe el color de texto para la ventana secundaria:", "black");
-
-    ventanaSecundaria.document.getElementById("mensaje").style.fontSize = tamañoTexto + "px";
-    ventanaSecundaria.document.getElementById("mensaje").style.color = colorTexto;
+   // Aplica las coordenadas si son válidas
+   if (ventanaSecundaria && !ventanaSecundaria.closed && !isNaN(nuevaAltura) && !isNaN(nuevaAnchura)) {
+      ventanaSecundaria.resizeTo(Number(nuevaAnchura), Number(nuevaAltura));
+   }
 }
