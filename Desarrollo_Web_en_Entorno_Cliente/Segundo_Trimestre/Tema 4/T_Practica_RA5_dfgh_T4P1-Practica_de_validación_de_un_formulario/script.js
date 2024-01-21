218916709312ecae2280c80de72570c4b8c2a3ed
ElPediatra <albertogomezmorales1994@gmail.com>
2024-01-21 14:19:14 +0100

    full script js y edit html

--- Desarrollo_Web_en_Entorno_Cliente/Segundo_Trimestre/Tema 4/T_Practica_RA5_dfgh_T4P1-Practica_de_validación_de_un_formulario/script.js
+++ Desarrollo_Web_en_Entorno_Cliente/Segundo_Trimestre/Tema 4/T_Practica_RA5_dfgh_T4P1-Practica_de_validación_de_un_formulario/script.js
@@ -5,6 +5,11 @@ window.onload = function() {
     var apellidos = document.getElementById('apellidos');
     var edad = document.getElementById('edad');
     var nif = document.getElementById('nif');
+    var email = document.getElementById('email');
+    var provincia = document.getElementById('provincia');
+    var fecha = document.getElementById('fecha');
+    var telefono = document.getElementById('telefono');
+    var hora = document.getElementById('hora');
 
     //Creo una variable para capturar los errores
     var errores = document.getElementById('errores');
@@ -19,6 +24,16 @@ window.onload = function() {
     edad.addEventListener('blur', validarEdad);
     //Creo un event listener para validar el campo "NIF" cuando no esté en foco, llamando a la función para validarla
     nif.addEventListener('blur', validarNIF);
+    //Creo un event listener para validar el campo "email" cuando no esté en foco, llamando a la función para validarlo
+    email.addEventListener('blur', validarEmail);
+    //Creo un event listener para validar el campo "provincia" cuando no esté en foco, llamando a la función para validarlo
+    provincia.addEventListener('blur', validarProvincia);
+    //Creo un event listener para validar el campo "fecha" cuando no esté en foco, llamando a la función para validarlo
+    fecha.addEventListener('blur', validarFecha);
+    //Creo un event listener para validar el campo "telefono" cuando no esté en foco, llamando a la función para validarlo
+    telefono.addEventListener('blur', validarTelefono);
+    //Creo un event listener para validar el campo "hora" cuando no esté en foco, llamando a la función para validarlo
+    hora.addEventListener('blur', validarHora);
 }
 
 //Función para cambiar el texto a mayúscula
@@ -66,6 +81,104 @@ function validarNIF(event) {
     //Si no se cumple la expresión regular, muestro mensaje de error y hago foco en el campo
     if (!regex.test(event.target.value)) {
         errores.textContent = 'El campo ' + event.target.id + ' debe tener el formato 12345678-A.';
+        event.target.focus();
+    } else {
+        //Si se ha corregido el campo, borro el mensaje de error
+        errores.textContent = '';
+    }
+}
+
+//Función para validar el correo/email del formulario
+function validarEmail(event) {
+    /* Uso esta expresión regular para validar los datos del email:
+        ^: Inicio de la cadena
+        [a-zA-Z0-9._%+-]+: Al menos un carácter alfanumérico o . _ % + -
+        @: Arroba
+        [a-zA-Z0-9.-]+: Al menos un carácter alfanumérico o . -
+        \.: Punto
+        [a-zA-Z]{2,}: Al menos dos caracteres alfabéticos
+        $: Fin de la cadena
+    */
+    var regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    //Si no se cumple la expresión regular, muestro mensaje de error y hago foco en el campo
+    if (!regex.test(event.target.value)) {
+        errores.textContent = 'El campo ' + event.target.id + ' debe tener el formato ejemplo@dominio.com.';
+        event.target.focus();
+    } else {
+        //Si se ha corregido el campo, borro el mensaje de error
+        errores.textContent = '';
+    }
+}
+
+//Función para validar las provincias
+function validarProvincia(event) {
+    // Si no se ha seleccionado ninguna provincia (el valor es '0') muestro mensaje y hago foco en el campo
+    if (event.target.value === '0') {
+        errores.textContent = 'Tienes que seleccionar una provincia.';
+        event.target.focus();
+    } else {
+        //Si se ha corregido el campo, borro el mensaje de error
+        errores.textContent = '';
+    }
+}
+
+//Función para validar la fecha
+function validarFecha(event) {
+    /* Uso esta expresión regular para validar los datos del email:
+        ^: Inicio de la cadena
+        (0[1-9]|[12][0-9]|3[01]): Día (01-31)
+        (\/|-): Separador (/ o -)
+        (0[1-9]|1[012]): Mes (01-12)
+        (\/|-): Separador (/ o -)
+        (19|20)\d\d: Año (1900-2099)
+        $: Fin de la cadena
+    */
+    var regex = /^(0[1-9]|[12][0-9]|3[01])(\/|-)(0[1-9]|1[012])\2(19|20)\d\d$/;
+
+    //Si no se cumple la expresión regular, muestro mensaje de error y hago foco en el campo
+    if (!regex.test(event.target.value)) {
+        errores.textContent = 'El campo ' + event.target.id + ' debe tener el formato dd/mm/aaaa o dd-mm-aaaa.';
+        event.target.focus();
+    } else {
+        //Si se ha corregido el campo, borro el mensaje de error
+        errores.textContent = '';
+    }
+}
+
+//Función para validar el teléfono del formulario
+function validarTelefono(event) {
+    /* Uso esta expresión regular para validar los datos del email:
+        ^: Inicio de la cadena
+        \d{9}: Exactamente 9 dígitos
+        $: Fin de la cadena
+    */
+    var regex = /^\d{9}$/;
+
+    //Si no se cumple la expresión regular, muestro mensaje de error y hago foco en el campo
+    if (!regex.test(event.target.value)) {
+        errores.textContent = 'El campo ' + event.target.id + ' debe tener exactamente 9 dígitos.';
+        event.target.focus();
+    } else {
+        //Si se ha corregido el campo, borro el mensaje de error
+        errores.textContent = '';
+    }
+}
+
+//Función para validar la hora del formulario
+function validarHora(event) {
+    /* Uso esta expresión regular para validar los datos del email:
+        ^: Inicio de la cadena
+        ([01]?[0-9]|2[0-3]): Hora (00-23)
+        : : Dos puntos
+        [0-5][0-9]: Minutos (00-59)
+        $: Fin de la cadena
+    */
+    var regex = /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/;
+
+    //Si no se cumple la expresión regular, muestro mensaje de error y hago foco en el campo
+    if (!regex.test(event.target.value)) {
+        errores.textContent = 'El campo ' + event.target.id + ' debe tener el formato hh:mm.';
         event.target.focus();
     } else {
         //Si se ha corregido el campo, borro el mensaje de error
