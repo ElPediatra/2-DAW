46fa32053ccdbd74c409c54bd7134159695cf6c6
elpediatra <albertogomezmorales1994@gmail.com>
2024-02-13 10:46:57 +0100

    Prueba AJAX - Need FIX

---
+++ Desarrollo_Web_en_Entorno_Cliente/Segundo_Trimestre/Tema 5/Ejemplo_Ajax_JSON/script.js
@@ -0,0 +1,20 @@
+var xmlhttp = new XMLHttpRequest();
+var url = "DWS.json";
+xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        var listaWebs = JSON.parse(this.responseText);
+        getWebs(listaWebs);
+    }
+};
+xmlhttp.open("GET", url, true);
+xmlhttp.send();
+
+function getWebs(arr) {
+    var out = "";
+    var i;
+    for (i = 0; i < arr.length; i++) {
+        out += '<a href="' + arr[i].url + '">' +
+        arr[i].display + '</a><br>';
+    }
+    document.getElementById("lasWebs").innerHTML = out;
+}
